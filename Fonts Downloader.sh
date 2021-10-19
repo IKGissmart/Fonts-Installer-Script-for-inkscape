f29fcd7ace58362ae38a72d5fb67a39808f9ebfb
IKGissmart <84079013+IKGissmart@users.noreply.github.com>
2021-10-19 12:29:09 +0500

    Update Fonts Downloader.sh

--- Fonts Downloader.sh
+++ Fonts Downloader.sh
@@ -2,7 +2,7 @@
 [ -d ~/.config/inkscape/fonts ] || mkdir -p ~/.config/inkscape/fonts
 cd ~/.config/inkscape/fonts
 echo -e "\e[1;32mDownloading Fonts\e[m"
-wget https://github.com/IKGissmart/Fonts-Installer-Script-for-inkscape/releases/download/FONTS/Fonts.script.zip -O Fonts
+wget "https://github.com/IKGissmart/Fonts-Installer-Script-for-inkscape/releases/download/FONTS/Fonts.package.zip" -O Fonts
 echo -e "\e[1;32mUnzipping The Fonts.zip\e[m"
 unzip Fonts
 echo -e"\e[1;32mUnzipping is Done\e[m"
